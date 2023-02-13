d381555a18443cf93da794ba1806811e4d06dc2a
Sareukil <wjstk5128@gmail.com>
2023-02-13 15:53:50 +0900

    asdsa

--- src/main/java/com/spring_boot_mybatis/project/WebConfig.java
+++ src/main/java/com/spring_boot_mybatis/project/WebConfig.java
@@ -14,9 +14,12 @@ public class WebConfig implements WebMvcConfigurer{
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // TODO Auto-generated method stub
         registry.addResourceHandler("/images/**")
-        .addResourceLocations("file:///C:/springWorkspace/product_images/",
-                                                "file:///C:/springWorkspace/upload/");
+        .addResourceLocations("file:///usr/local/project/product_images/");
 
+       /* registry.addResourceHandler("/images/**")
+        .addResourceLocations("file:///C:/springWorkspace/product_images/",
+                                                "file:///C:/springWorkspace/upload/");*/
+                                                
         registry.addResourceHandler("/audio/**")
         .addResourceLocations("file:///C:/springWorkspace/upload/");
     }
